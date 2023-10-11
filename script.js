fb9255eaaa07f048eb0d81bf8f1671f078475227
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-10-11 18:02:51 -0400

    Some Clean Up

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let hours = document.getElementById("hours");
 let mins = document.getElementById("mins");
-let date = document.getElementById("date");
-let month = document.getElementById("month");
-let year = document.getElementById("year");
 let secs = document.getElementById("secs");
+let month = document.getElementById("month");
+let date = document.getElementById("date");
+let year = document.getElementById("year");
 
 
 let l = new Date();
@@ -13,17 +13,17 @@ setInterval(() => {
 
     let currentTime = new Date();
 
-    hours.innerHTML = (currentTime.getHours() < 10 ? "0"  : "") + (currentTime.getHours());
-    mins.innerHTML = (currentTime.getMinutes() < 10 ? "0"  : "") + (currentTime.getMinutes());
-    secs.innerHTML = (currentTime.getSeconds() < 10 ? "0"  : "") + (currentTime.getSeconds());
+    hours.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + (currentTime.getHours());
+    mins.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + (currentTime.getMinutes());
+    secs.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + (currentTime.getSeconds());
 
-    month.innerHTML = (currentTime.getMonth() + 1 < 10 ? "0"  : "") + (currentTime.getMonth() + 1);
-date.innerHTML = (currentTime.getDate() < 10 ? "0"  : "") + (currentTime.getDate());
-year.innerHTML = (currentTime.getFullYear() < 10 ? "0"  : "") + (currentTime.getFullYear());
+    month.innerHTML = (currentTime.getMonth() + 1 < 10 ? "0" : "") + (currentTime.getMonth() + 1);
+    date.innerHTML = (currentTime.getDate() < 10 ? "0" : "") + (currentTime.getDate());
+    year.innerHTML = (currentTime.getFullYear() < 10 ? "0" : "") + (currentTime.getFullYear());
 
-},1000)
+}, 1000)
 
-   
+
 
 
 
