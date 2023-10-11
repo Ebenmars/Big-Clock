df4d3143bf0b2d0a86fca19e1e296c14eb53a49c
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-10-11 17:59:51 -0400

    Added Time And Single Interger Feature
    JavaScript has a thing were they do 0 - 11. Remember to add + 1 t the months

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let hours = document.getElementById("hours");
 let mins = document.getElementById("mins");
-let secs = document.getElementById("secs");
 let date = document.getElementById("date");
 let month = document.getElementById("month");
 let year = document.getElementById("year");
-
-let currentTime = new Date();
-
-// console.log(currentTime);
+let secs = document.getElementById("secs");
 
 
-date.innerHTML = currentTime.getMonth();
-month.innerHTML = currentTime.getDay();
-year.innerHTML = currentTime.getFullYear();
-hours.innerHTML = currentTime.getHours();
-mins.innerHTML = currentTime.getMinutes();
-secs.innerHTML = currentTime.getSeconds();
+let l = new Date();
+console.log(l.getMonth() + 1);
+
+setInterval(() => {
+
+    let currentTime = new Date();
+
+    hours.innerHTML = (currentTime.getHours() < 10 ? "0"  : "") + (currentTime.getHours());
+    mins.innerHTML = (currentTime.getMinutes() < 10 ? "0"  : "") + (currentTime.getMinutes());
+    secs.innerHTML = (currentTime.getSeconds() < 10 ? "0"  : "") + (currentTime.getSeconds());
+
+    month.innerHTML = (currentTime.getMonth() + 1 < 10 ? "0"  : "") + (currentTime.getMonth() + 1);
+date.innerHTML = (currentTime.getDate() < 10 ? "0"  : "") + (currentTime.getDate());
+year.innerHTML = (currentTime.getFullYear() < 10 ? "0"  : "") + (currentTime.getFullYear());
+
+},1000)
+
+   
+
+
+
+
