f83b8fab0933a792630dfd8894365181d8bf1bb4
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-10-11 17:37:23 -0400

    Added Calendar Date & Added Time

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let hours = document.getElementById("hours");
 let mins = document.getElementById("mins");
 let secs = document.getElementById("secs");
-
+let date = document.getElementById("date");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
 
 let currentTime = new Date();
 
-console.log
+// console.log(currentTime);
+
+
+date.innerHTML = currentTime.getMonth();
+month.innerHTML = currentTime.getDay();
+year.innerHTML = currentTime.getFullYear();
+hours.innerHTML = currentTime.getHours();
+mins.innerHTML = currentTime.getMinutes();
+secs.innerHTML = currentTime.getSeconds();
